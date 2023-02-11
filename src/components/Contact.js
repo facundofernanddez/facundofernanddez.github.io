d1cab2db9f0f730e58ee489e61dd59091dd99f5e
Facundo Fernandez <ff96_@hotmail.com>
2023-02-11 11:37:19 -0300

    update: contact and projects

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,3 +1,38 @@
 export const Contact = () => {
-  return <h2>Soy el contact</h2>;
+  return (
+    <>
+      <div className="row">
+        <div className="col-6">
+          <div className="card">
+            <div className="card-title">Mis redes sociales</div>
+          </div>
+        </div>
+        <div className="col-6">
+          <form className="card">
+            <div className="mb-3">
+              <label for="exampleFormControlInput1" className="form-label">
+                Email
+              </label>
+              <input
+                type="email"
+                class="form-control"
+                id="exampleFormControlInput1"
+                placeholder="Email"
+              />
+            </div>
+            <div className="mb-3">
+              <label for="exampleFormControlTextarea1" className="form-label">
+                Mensaje
+              </label>
+              <textarea
+                className="form-control"
+                id="exampleFormControlTextarea1"
+                rows="3"
+              ></textarea>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 };
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,3 +1,54 @@
+import calculadora from "../images/calculadora.png";
+
 export const Projects = () => {
-  return <h2>Este es la pag de proyectos</h2>;
+  const projects = [
+    {
+      title: "my project 1",
+      image: "https://i.ytimg.com/vi/Q7bbdJpiLRs/maxresdefault.jpg",
+      overview: "description of project 1",
+    },
+    {
+      title: "my project 2",
+      image:
+        "https://www.lasirena.la/__export/1529782056181/sites/debate/img/2018/02/09/gatos_crop1513745237964_crop1516831635779_crop1518189988779.jpg_242310155.jpg",
+      overview: "description of project 2",
+    },
+    {
+      title: "my project 2",
+      image:
+        "https://www.bunko.pet/__export/1619479458417/sites/debate/img/2021/04/26/curiosidades_de_los_gatos_crop1619478790261.jpg_1532751094.jpg",
+      overview: "description of project 2",
+    },
+    {
+      title: "my project 2",
+      image:
+        "https://3.bp.blogspot.com/-WoMyw3HPzyE/V1em-csdnBI/AAAAAAAAAuQ/sNe05qdDa7wyaloRJmfWTjj8Sg9UEVd5ACKgB/s1600/cat-1046544_1920.jpg",
+      overview: "description of project 2",
+    },
+    {
+      title: "my project 2",
+      image: "https://i.ytimg.com/vi/Hlz7ass3gRk/maxresdefault.jpg",
+      overview: "description of project 2",
+    },
+  ];
+
+  return (
+    <>
+      <div className="row">
+        {projects.map((oneProject, index) => {
+          return (
+            <div className="col-4" key={index}>
+              <div className="card my-4">
+                <img src={oneProject.image} className="card-img" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">{oneProject.title}</h5>
+                  <p className="card-text">{oneProject.overview}</p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 };
