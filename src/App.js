437a9f4452075b004f3d941f8bb4efc99ed56caa
Facundo Fernandez <ff96_@hotmail.com>
2023-04-16 17:34:05 -0300

    modifying footer

--- src/App.js
+++ src/App.js
@@ -19,8 +19,8 @@ function App() {
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
+        <Footer />
       </div>
-      <Footer />
     </BrowserRouter>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,9 @@
 export const Footer = () => {
   return (
-    <footer>
-      <h5 className="opacity-50 text-center text-light my-4">
-        Facundo Fernandez.
-      </h5>
+    <footer className="position-relative justify-content-center bottom-0 start-50 translate-middle-x">
+      <p className="opacity-75 text-center text-white my-2 py-2">
+        Copyright © 2023 Facundo Fernandez | Todos los derechos reservados.
+      </p>
     </footer>
   );
 };
