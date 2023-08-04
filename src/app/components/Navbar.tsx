faf23fbc67684f52d0b9f497bd11c2ea11a9cf4e
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-08-03 22:46:23 -0300

    change contact for guestbook

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -113,10 +113,10 @@ export default function Navbar() {
                 Home
               </Link>
               <Link
-                href="/contact"
+                href="/guestbook"
                 prefetch
                 className={`${
-                  pathname == "/contact"
+                  pathname == "/guestbook"
                     ? "bg-teal-50 border-teal-500 text-teal-500 block pl-3 pr-4 py-2 border-l-4 text-base font-medium dark:bg-gray-800"
                     : "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-teal-500 block pl-3 pr-4 py-2 dark:hover:bg-gray-700 border-l-4 text-base font-medium dark:text-white"
                 }`}
