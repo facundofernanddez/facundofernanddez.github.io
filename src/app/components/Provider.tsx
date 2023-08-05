447ca0e0590e3b65150fe4e6db4139f1ad6106d3
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-08-04 22:36:17 -0300

    refact

--- src/app/components/Provider.tsx
+++ src/app/components/Provider.tsx
@@ -4,13 +4,15 @@ import { ReactNode } from "react";
 import { ThemeProvider } from "next-themes";
 import { createContext, useState } from "react";
 
-export const LanguageContext = createContext("en" || "es");
+export const LanguageContext = createContext({});
 
 export function Provider({ children }: { children: ReactNode }) {
-  const [lang, setLang] = useState("en" || "es");
+  const [lang, setLang] = useState({
+    lang: "en" || "es",
+  });
 
   return (
-    <LanguageContext.Provider value={lang}>
+    <LanguageContext.Provider value={{ lang, setLang }}>
       <ThemeProvider attribute="class">{children}</ThemeProvider>;
     </LanguageContext.Provider>
   );
