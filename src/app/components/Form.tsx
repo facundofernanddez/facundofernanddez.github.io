614987e0e2b25ec9ea2f6274cf38b7bd736487ed
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-09-05 21:39:13 -0300

    fix empty messages bug

--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useContext, useRef } from "react";
+import { ChangeEvent, useContext, useRef, useState } from "react";
 import { postEntry } from "../action";
 import { experimental_useFormStatus as useFormStatus } from "react-dom";
 import { LanguageContext } from "./LanguageProvider";
@@ -8,11 +8,34 @@ import { LanguageContext } from "./LanguageProvider";
 export default function Form() {
   const formRef = useRef<HTMLFormElement>(null);
   const { pending } = useFormStatus();
+  const [buttonPending, setButtonPending] = useState(false);
   const { lang } = useContext(LanguageContext);
 
   const actionPost = async (formData: FormData) => {
+    const formEntry = formData.get("entry") as string;
+
+    const entry = formEntry.trim();
+
+    if (entry.length === 0) {
+      return;
+    }
+
+    pending;
+
     await postEntry(formData);
     formRef.current?.reset();
+  };
+
+  const changeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    const inputValue = e.currentTarget.value;
+
+    if (inputValue.trim().length === 0) {
+      setButtonPending(true);
+    }
+
+    if (inputValue.trim().length > 0) {
+      setButtonPending(false);
+    }
   };
 
   return (
@@ -30,6 +53,7 @@ export default function Form() {
       >
         <input
           type="text"
+          onChange={(e) => changeHandler(e)}
           placeholder={
             lang.language === "en" ? "Your message..." : "Tu mensaje..."
           }
@@ -41,7 +65,8 @@ export default function Form() {
 
         <button
           type="submit"
-          disabled={pending}
+          disabled={buttonPending}
+          style={{ opacity: buttonPending ? 0.5 : 1 }}
           className="flex items-center justify-center absolute right-2 mt-1 font-medium h-7 bg-teal-500/30 text-neutral-900 dark:text-neutral-100 rounded w-16"
         >
           {lang.language === "en" ? "Send" : "Enviar"}
