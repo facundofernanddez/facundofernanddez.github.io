de0fe6d6fae9e40498dbc6e1e9c3b5a78617f166
Alejandro Facundo Fernandez <91287409+facundofernanddez@users.noreply.github.com>
2025-02-16 23:34:09 -0300

    update current year

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,9 +1,11 @@
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <div className="divide-y divide-gray-100 dark:divide-gray-700">
       <footer className="text-center">
         <p className="text-gray-600 text-sm mb-2">
-          Copyright &copy; 2023-2024 Facundo Fernandez
+          Copyright &copy; 2023-{currentYear} Facundo Fernandez
         </p>
       </footer>
     </div>
